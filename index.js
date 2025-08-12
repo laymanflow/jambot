bb769d91c00198c9240dbc504a37263a40639d82
laymanflow <epetrie337@gmail.com>
2025-08-11 21:42:54 -0400

    Added script to deploy slash commands

--- index.js
+++ index.js
@@ -22,13 +22,15 @@ client.login(token);
 //Slash commands
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, './commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
+	console.log("Commands path: ", commandsPath);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	console.log("Command files: ", commandFiles);
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
@@ -41,7 +43,24 @@ for (const folder of commandFolders) {
 	}
 }
 
-client.on(Events.InteractionCreate, interaction => {
+client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
-	console.log(interaction);
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		}
+	}
 });
